63ea493eab914427bb2d4c2bc7ab63bf9fc5b0b7
Paulo Eduardo Neves <pauloneves@gmail.com>
2021-09-23 14:01:56 -0300

    Gera gráfico de nº alunos por ano

--- const.py
+++ const.py
@@ -1,20 +1,17 @@
-CO_STO_AGOS_NL =   33087679 
-CO_STO_AGO_INST = 33176825
-CO_STO_AGO = 33020566
+CO_ELEVA = 33178860
+CO_PARQUE = 33065837
 CO_SAO_BENTO = 33062633
 CO_SAO_VICENTE = 33063648
-CO_PARQUE = 33065837
-CO_ELEVA = 33178860
-CO_ST_INACIO = 33063729
 CO_SAO_VICENTE = 33063648
+CO_ST_INACIO = 33063729
+CO_STO_AGO = 33020566
+CO_STO_AGO_INST = 33176825
+CO_STO_AGOS_NL = 33087679
 
 
-CO_MUN = 3304557 # Rio de Janeiro
-CO_UF_RIO = 33 # RJ
+CO_MUN = 3304557  # Rio de Janeiro
+CO_UF_RIO = 33  # RJ
 
-MIN_ALUNOS = 30 # Mínimo de alunos fazendo Enem para considerar na análise
+MIN_ALUNOS = 30  # Mínimo de alunos fazendo Enem para considerar na análise
 NUM_MELHORES = 60
-ANO_ULT=2019 # último ano a processar
-
-
-
+ANO_ULT = 2019  # último ano a processar
